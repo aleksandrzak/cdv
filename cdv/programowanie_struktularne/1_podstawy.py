33e26f6a078072657a7abd7c184da50d1f882f5c
Krystian Aleksandrzak <kaleksandrzak@edu.cdv.pl>
2019-03-25 12:53:44 +0100

    phyton

--- cdv/programowanie_struktularne/1_podstawy.py
+++ cdv/programowanie_struktularne/1_podstawy.py
@@ -1,2 +1,43 @@
 print("CDV")
 print(2)
+
+#potęga
+potega = 2 ** 10
+print (potega)
+
+tekst = "CDV"
+
+
+print(tekst * 2)
+
+#pobieranie danych z klawiatury
+imie = input()
+print("twoje imie podane z klawiatury: " + imie)
+
+nazwisko = input()
+print("twoje imie: " + imie + ", twoje nazwisko: " + nazwisko)
+
+
+dlugosc = len(nazwisko)
+print(type(nazwisko))
+print(type(dlugosc))
+print("dlugosc nazwiska: ", dlugosc)
+dlugosc = str(dlugosc)	#konwersja danych int/str
+print("dlugosc nazwiska: " + dlugosc)
+print()
+
+#uzytkownik wpisuje z klawiatury swoj wiek, wyswietl dane w formacie twoj wiek: ... lat
+
+print("\nPodaj swoj wiek: ", end="")
+wiek = input()
+print("twoj wiek: ", wiek, " lat")
+
+
+
+
+
+
+
+
+
+
