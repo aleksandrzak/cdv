b6d256395c3553991380b49a1dfd5cde2529880b
aleksandrzak <48910989+aleksandrzak@users.noreply.github.com>
2019-04-29 13:45:25 +0200

    Add files via upload

---
+++ cdv/programowanie_struktularne/10_petle.py
@@ -0,0 +1,48 @@
+#pętla for
+
+colors = ['red', 'green', 'blue', 'magneta']
+print(colors)
+print(type(colors))
+
+for color in colors:
+    print(color)
+    pass
+print('\n')
+
+#wypisz tekst
+string = 'CDV - uczelnia ludzi ciekawych'
+
+for letter in string:
+    print(letter,end=" ")
+    pass
+print()
+#wypisz liczby od 1-10
+for number in range(1, 11):
+    print(number,end=" ")
+    pass
+print()
+
+###wypisz elementy z listy do momentu wartosci 'end', 'q', 'quit'
+
+lista = ['red', 'green', 'end', 'blue', 'q', 'magneta', 'quit']
+
+for list in lista:
+    print(lista)
+#    if (lista = 'q') break
+
+    pass
+print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
